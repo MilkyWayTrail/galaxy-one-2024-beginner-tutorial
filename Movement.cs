4699495966e7cc08920b6e331c223cd26592f830
Victor Kwong <victorkwong128@gmail.com>
2024-07-27 12:37:40 -0400

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour 
+public class Movement : MonoBehaviour 
 {
     public float speed = 5.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
